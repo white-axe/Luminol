fca020cf5fad2b0c2c0b510f3dd6a2b3712ded9b
刘皓 <whiteaxe@tuta.io>
2026-09-13 10:51:28 -0400

    Move `EnumComboBox` and `OptionalIdComboBox` into separate files

---
+++ crates/ui/src/components/enum_combo_box.rs
@@ -0,0 +1,175 @@
+// Copyright (C) 2024 Melody Madeline Lyons
+//
+// This file is part of Luminol.
+//
+// Luminol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Luminol is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Luminol.  If not, see <http://www.gnu.org/licenses/>.
+//
+//     Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this Program, or any covered work, by linking or combining
+// it with Steamworks API by Valve Corporation, containing parts covered by
+// terms of the Steamworks API by Valve Corporation, the licensors of this
+// Program grant you additional permission to convey the resulting work.
+
+use super::UiExt;
+
+trait EnumCast {
+    type Enum;
+    type Reference;
+    fn get_mut(&mut self) -> &mut Self::Reference;
+    fn cast(&self) -> Option<(std::mem::Discriminant<Self::Enum>, String)>;
+}
+
+pub struct TrivialEnumCast<'a, T>(&'a mut T);
+
+impl<T> EnumCast for TrivialEnumCast<'_, T>
+where
+    T: ToString,
+{
+    type Enum = T;
+    type Reference = T;
+
+    fn get_mut(&mut self) -> &mut Self::Reference {
+        self.0
+    }
+
+    fn cast(&self) -> Option<(std::mem::Discriminant<Self::Enum>, String)> {
+        Some((std::mem::discriminant(self.0), self.0.to_string()))
+    }
+}
+
+pub struct TryIntoEnumCast<'a, T, R>(&'a mut R, std::marker::PhantomData<T>);
+
+impl<T, R, E> EnumCast for TryIntoEnumCast<'_, T, R>
+where
+    T: ToString,
+    R: TryInto<T, Error = E> + Clone,
+{
+    type Enum = T;
+    type Reference = R;
+
+    fn get_mut(&mut self) -> &mut Self::Reference {
+        self.0
+    }
+
+    fn cast(&self) -> Option<(std::mem::Discriminant<Self::Enum>, String)> {
+        let value = self.0.clone().try_into().ok()?;
+        Some((std::mem::discriminant(&value), value.to_string()))
+    }
+}
+
+pub struct EnumComboBox<C, H> {
+    id_source: H,
+    reference: C,
+
+    max_width: f32,
+    wrap_mode: egui::TextWrapMode,
+}
+
+impl<'a, T, H> EnumComboBox<TrivialEnumCast<'a, T>, H>
+where
+    T: ToString + strum::IntoEnumIterator,
+    H: std::hash::Hash,
+{
+    /// Creates a combo box that can be used to change the variant of an enum `T` that implements
+    /// `ToString + strum::IntoEnumIterator`.
+    pub fn new(id_source: H, reference: &'a mut T) -> Self {
+        Self::new_impl(id_source, TrivialEnumCast(reference))
+    }
+}
+
+impl<'a, T, R, H, E> EnumComboBox<TryIntoEnumCast<'a, T, R>, H>
+where
+    T: Into<R> + ToString + strum::IntoEnumIterator,
+    R: TryInto<T, Error = E> + Clone,
+    H: std::hash::Hash,
+{
+    /// Creates a combo box that can be used to change a value that can be converted to/from an enum
+    /// `T` that implements `ToString + strum::IntoEnumIterator`.
+    pub fn new_with_conversion(
+        enum_type: std::marker::PhantomData<T>,
+        id_source: H,
+        reference: &'a mut R,
+    ) -> Self {
+        Self::new_impl(id_source, TryIntoEnumCast(reference, enum_type))
+    }
+}
+
+impl<C, H> EnumComboBox<C, H> {
+    fn new_impl(id_source: H, reference: C) -> Self {
+        Self {
+            id_source,
+            reference,
+            max_width: f32::INFINITY,
+            wrap_mode: egui::TextWrapMode::Wrap,
+        }
+    }
+
+    pub fn max_width(mut self, max_width: f32) -> Self {
+        self.max_width = max_width;
+        self
+    }
+
+    pub fn wrap_mode(mut self, wrap_mode: egui::TextWrapMode) -> Self {
+        self.wrap_mode = wrap_mode;
+        self
+    }
+}
+
+impl<C, T, R, H> egui::Widget for EnumComboBox<C, H>
+where
+    C: EnumCast<Enum = T, Reference = R>,
+    T: Into<R> + ToString + strum::IntoEnumIterator,
+    H: std::hash::Hash,
+{
+    fn ui(mut self, ui: &mut egui::Ui) -> egui::Response {
+        let mut changed = false;
+        let available_width = ui.available_width() - ui.spacing().item_spacing.x;
+        let width = self.max_width.min(available_width);
+        let (discriminant, text) = if let Some((discriminant, text)) = self.reference.cast() {
+            (Some(discriminant), text)
+        } else {
+            (None, Default::default())
+        };
+        let mut response = egui::ComboBox::from_id_salt(&self.id_source)
+            .wrap()
+            .width(width)
+            .selected_text(text)
+            .show_ui(ui, |ui| {
+                ui.style_mut().wrap_mode = Some(self.wrap_mode);
+
+                for (i, variant) in T::iter().enumerate() {
+                    ui.with_stripe(i % 2 != 0, |ui| {
+                        if ui
+                            .selectable_label(
+                                discriminant.is_some_and(|discriminant| {
+                                    discriminant == std::mem::discriminant(&variant)
+                                }),
+                                variant.to_string(),
+                            )
+                            .clicked()
+                        {
+                            *self.reference.get_mut() = variant.into();
+                            changed = true;
+                        }
+                    });
+                }
+            })
+            .response;
+        if changed {
+            response.mark_changed();
+        }
+        response
+    }
+}
---
+++ crates/ui/src/components/optional_id_combo_box.rs
@@ -0,0 +1,330 @@
+// Copyright (C) 2024 Melody Madeline Lyons
+//
+// This file is part of Luminol.
+//
+// Luminol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Luminol is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Luminol.  If not, see <http://www.gnu.org/licenses/>.
+//
+//     Additional permission under GNU GPL version 3 section 7
+//
+// If you modify this Program, or any covered work, by linking or combining
+// it with Steamworks API by Valve Corporation, containing parts covered by
+// terms of the Steamworks API by Valve Corporation, the licensors of this
+// Program grant you additional permission to convey the resulting work.
+
+use super::UiExt;
+use itertools::Itertools;
+
+pub trait IdCast
+where
+    Self: Sized,
+{
+    fn from_id(value: usize) -> Option<Self>;
+    fn to_id(&self) -> Option<usize>;
+}
+
+macro_rules! impl_into_optional_id {
+    ($($primitive:ty),* $(,)?) => {
+        $(
+            impl IdCast for $primitive {
+                fn from_id(value: usize) -> Option<Self> {
+                    value.try_into().ok()
+                }
+                fn to_id(&self) -> Option<usize> {
+                    (*self).try_into().ok()
+                }
+            }
+        )*
+    };
+}
+
+impl_into_optional_id!(i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize);
+
+pub struct OptionalIdComboBox<'a, R, I, H, F> {
+    id_source: H,
+    reference: &'a mut R,
+    id_iter: I,
+    formatter: F,
+    search_needs_update: bool,
+    allow_none: bool,
+}
+
+impl<'a, R, I, H, F> OptionalIdComboBox<'a, R, I, H, F>
+where
+    I: Iterator<Item = usize> + Clone,
+    H: std::hash::Hash,
+    F: Fn(usize) -> String,
+{
+    /// Creates a combo box that can be used to change the ID of an `optional_id` field in the data
+    /// cache.
+    pub fn new(
+        update_state: &luminol_core::UpdateState<'_>,
+        id_source: H,
+        reference: &'a mut R,
+        id_iter: I,
+        formatter: F,
+    ) -> Self {
+        Self {
+            id_source,
+            reference,
+            id_iter,
+            formatter,
+            search_needs_update: *update_state.modified_during_prev_frame,
+            allow_none: true,
+        }
+    }
+
+    fn ui_inner(
+        self,
+        ui: &mut egui::Ui,
+        formatter: impl Fn(&Self) -> String,
+        f: impl FnOnce(Self, &mut egui::Ui, Vec<usize>, bool, bool) -> bool,
+    ) -> egui::Response {
+        let source = egui::Id::new(&self.id_source);
+        let state_id = ui.make_persistent_id(source).with("OptionalIdComboBox");
+        let popup_id = ui.make_persistent_id(source).with("popup");
+        let is_popup_open = egui::Popup::is_id_open(ui.ctx(), popup_id);
+
+        let mut changed = false;
+        let inner_response = egui::ComboBox::from_id_salt(&self.id_source)
+            .wrap()
+            .width(ui.available_width() - ui.spacing().item_spacing.x)
+            .selected_text(formatter(&self))
+            .show_ui(ui, |ui| {
+                // Get cached search string and search matches from egui memory
+                let (mut search_string, search_matched_ids_lock) = is_popup_open
+                    .then(|| ui.data(|d| d.get_temp(state_id)))
+                    .flatten()
+                    .unwrap_or_else(|| {
+                        (
+                            String::new(),
+                            // We use a mutex here because if we just put the Vec directly into
+                            // memory, egui will clone it every time we get it from memory
+                            std::sync::Arc::new(parking_lot::Mutex::new(
+                                self.id_iter.clone().collect_vec(),
+                            )),
+                        )
+                    });
+                let mut search_matched_ids = search_matched_ids_lock.lock();
+
+                let search_box_response =
+                    ui.add(egui::TextEdit::singleline(&mut search_string).hint_text("Search 🔎"));
+
+                ui.add_space(ui.spacing().item_spacing.y);
+
+                // If the combo box popup was not open the previous frame and was opened this
+                // frame, focus the search box
+                if !is_popup_open {
+                    search_box_response.request_focus();
+                }
+
+                let search_box_clicked = search_box_response.clicked()
+                    || search_box_response.secondary_clicked()
+                    || search_box_response.middle_clicked()
+                    || search_box_response.clicked_by(egui::PointerButton::Extra1)
+                    || search_box_response.clicked_by(egui::PointerButton::Extra2);
+
+                // If the user edited the contents of the search box or if the data cache changed
+                // this frame, recalculate the search results
+                let search_needs_update = self.search_needs_update || search_box_response.changed();
+                if search_needs_update {
+                    let matcher = fuzzy_matcher::skim::SkimMatcherV2::default();
+                    search_matched_ids.clear();
+                    search_matched_ids.extend(self.id_iter.clone().filter(|id| {
+                        matcher
+                            .fuzzy(&(self.formatter)(*id), &search_string, false)
+                            .is_some()
+                    }));
+                }
+
+                let button_height = ui.spacing().interact_size.y.max(
+                    ui.text_style_height(&egui::TextStyle::Button)
+                        + 2. * ui.spacing().button_padding.y,
+                );
+                egui::ScrollArea::vertical().show_rows(
+                    ui,
+                    button_height,
+                    search_matched_ids.len() + self.allow_none as usize,
+                    |ui, range| {
+                        let first_row_is_faint = range.clone().start % 2 != 0;
+                        let show_none = self.allow_none && range.clone().start == 0;
+                        let ids = range
+                            .filter_map(|i| {
+                                if self.allow_none {
+                                    (i != 0).then(|| search_matched_ids[i - 1])
+                                } else {
+                                    Some(search_matched_ids[i])
+                                }
+                            })
+                            .collect_vec();
+                        changed = f(self, ui, ids, first_row_is_faint, show_none);
+                    },
+                );
+
+                // Save the search string and the search results back into egui memory
+                drop(search_matched_ids);
+                ui.data_mut(|d| d.insert_temp(state_id, (search_string, search_matched_ids_lock)));
+
+                search_box_clicked
+            });
+        let mut response = inner_response.response;
+
+        if inner_response.inner == Some(true) {
+            // Force the combo box to stay open if the search box was clicked
+            egui::Popup::open_id(ui.ctx(), popup_id);
+        } else if inner_response.inner.is_none()
+            && ui.data(|d| {
+                d.get_temp::<String>(state_id)
+                    .is_some_and(|s| !s.is_empty())
+            })
+        {
+            // Clear the search box if the combo box is closed
+            ui.data_mut(|d| d.insert_temp(state_id, String::new()));
+        }
+
+        if changed {
+            response.mark_changed();
+        }
+        response
+    }
+}
+
+impl<T, I, H, F> OptionalIdComboBox<'_, Option<T>, I, H, F>
+where
+    T: IdCast,
+    I: Iterator<Item = usize> + Clone,
+    H: std::hash::Hash,
+    F: Fn(usize) -> String,
+{
+    /// Enables or disables selecting the "(None)" option in the combo box. Defaults to `true`.
+    pub fn allow_none(mut self, value: bool) -> Self {
+        self.allow_none = value;
+        self
+    }
+}
+
+impl<T, I, H, F> egui::Widget for OptionalIdComboBox<'_, Option<T>, I, H, F>
+where
+    T: IdCast,
+    I: Iterator<Item = usize> + Clone,
+    H: std::hash::Hash,
+    F: Fn(usize) -> String,
+{
+    fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+        let mut changed = false;
+
+        self.ui_inner(
+            ui,
+            |this| {
+                if let Some(id) = this.reference.as_ref() {
+                    if let Some(id) = id.to_id() {
+                        (this.formatter)(id)
+                    } else {
+                        "".into()
+                    }
+                } else {
+                    "(None)".into()
+                }
+            },
+            |this, ui, ids, first_row_is_faint, show_none| {
+                ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Truncate);
+
+                if show_none
+                    && ui
+                        .with_stripe(false, |ui| {
+                            ui.selectable_label(this.reference.is_none(), "(None)")
+                        })
+                        .inner
+                        .clicked()
+                {
+                    *this.reference = None;
+                    changed = true;
+                }
+
+                let reference_id = this.reference.as_ref().and_then(|inner| inner.to_id());
+
+                let mut is_faint = first_row_is_faint != show_none;
+
+                for id in ids {
+                    ui.with_stripe(is_faint, |ui| {
+                        ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Truncate);
+
+                        if ui
+                            .selectable_label(reference_id == Some(id), (this.formatter)(id))
+                            .clicked()
+                        {
+                            if let Some(new_value) = IdCast::from_id(id) {
+                                *this.reference = Some(new_value);
+                                changed = true;
+                            }
+                        }
+                    });
+                    is_faint = !is_faint;
+                }
+
+                changed
+            },
+        )
+    }
+}
+
+impl<T, I, H, F> egui::Widget for OptionalIdComboBox<'_, T, I, H, F>
+where
+    T: IdCast,
+    I: Iterator<Item = usize> + Clone,
+    H: std::hash::Hash,
+    F: Fn(usize) -> String,
+{
+    fn ui(mut self, ui: &mut egui::Ui) -> egui::Response {
+        self.allow_none = false;
+
+        let mut changed = false;
+
+        self.ui_inner(
+            ui,
+            |this| {
+                if let Some(id) = this.reference.to_id() {
+                    (this.formatter)(id)
+                } else {
+                    "".into()
+                }
+            },
+            |this, ui, ids, first_row_is_faint, _| {
+                ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Truncate);
+
+                let reference_id = this.reference.to_id();
+
+                let mut is_faint = first_row_is_faint;
+
+                for id in ids {
+                    ui.with_stripe(is_faint, |ui| {
+                        ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Truncate);
+
+                        if ui
+                            .selectable_label(reference_id == Some(id), (this.formatter)(id))
+                            .clicked()
+                        {
+                            if let Some(new_value) = IdCast::from_id(id) {
+                                *this.reference = new_value;
+                                changed = true;
+                            }
+                        }
+                    });
+                    is_faint = !is_faint;
+                }
+
+                changed
+            },
+        )
+    }
+}
